ce96ad49fc2dabc4cb9ce62e626c30cf8a056d6b
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-01-29 21:29:22 +0300

    -+*/

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿double n = 7, n1 = -8.5;
+﻿double n = 7, n1 = -15.5;
 Console.Write($"{n} + ({n1}) = {n + n1}\n");
 Console.WriteLine($"{n} - ({n1}) = {n - n1}");
 Console.WriteLine($"{n} * ({n1}) = {n * n1}");
@@ -13,4 +13,13 @@ double M2;
 M1 = Math.Round(M);
 M2 = Math.Abs(M);
 Console.WriteLine($"M = {M1}\nМодуль числа = {M2}");
+Console.Write($"M = ");
 Console.WriteLine(Math.Round(M, 2)); // 2 -это кол-во знаков после запятой
+double C = n / n1;
+C += 10;// тут хуярим любое число 
+C++; // это просто +1 
+C -= 2;
+C *= 1.5;
+C /= 2;
+Console.Write($"C = ");
+Console.WriteLine(Math.Round(C, 2));
