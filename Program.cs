6be13ef6be1a1f241b4e61e9b388e5a306b7217b
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-01-29 20:16:29 +0300

    Calculator

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿double n = 10;
+double n1 = 7;
+Console.WriteLine($"{n} + {n1} = {n + n1}");
+Console.WriteLine($"{n} - {n1} = {n - n1}");
+Console.WriteLine($"{n} * {n1} = {n * n1}");
+Console.WriteLine($"{n} : {n1} = {n / n1}");
