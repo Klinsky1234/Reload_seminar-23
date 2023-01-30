3ece0445349b8d0f611c68440b44b7aa85f15fff
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-01-31 01:32:26 +0300

    T6

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿/* 
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+double a = 0;
+Console.WriteLine("Enter a number: ");
+double.TryParse(Console.ReadLine()!, out a);
+Console.Write($"{a} -> ");
+for (int i = 2; i < a; i++)
+{
+    if (i % 2 == 0)
+        Console.Write($"{i} ");
+}
+
