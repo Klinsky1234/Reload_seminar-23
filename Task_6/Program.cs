4a8ab4609b4efe21a3b5aa7a8f6815df92964c19
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-01-31 01:17:34 +0300

    T6

---
+++ Task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет */
+double a = 0;
+Console.WriteLine("Enter a number: ");
+double.TryParse(Console.ReadLine()!, out a);
+
+if (a % 2 == 0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
